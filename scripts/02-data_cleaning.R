dc1cea01529b0177bd857d84243592fced9a196d
DianaShen1224 <diana.shen@utoronto.ca>
2024-09-25 16:51:59 -0400

    add test to cleaned script

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -24,3 +24,25 @@ cleaned_data <-
   mutate(date=lubridate::ymd(paste(year,month,day,sep="-")))
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
+
+#### Test cleaned data ####
+data_shelter <- read_csv("data/raw_data/toronto_shelters_2023.csv")
+# Test for negative numbers
+## function for testing the negative numbers
+check_non_negative <- function(data) {
+  negative_test <- data %>%
+    summarise_all(~ min(.)) %>%
+    gather(key = "column", value = "min_value") %>%
+    filter(min_value < 0)
+  if (nrow(negative_test) == 0) {
+    print("All columns have non-negative values.")
+  } else {
+    print("Negative values found in the following columns:") ##print when there's negative numbers
+    print(negative_test)
+  }
+}
+## check raw data
+check_non_negative(data_shelter )
+
+# Test for NAs
+all(is.na(data_shelter)) ## If there is missing values in our raw dataset
